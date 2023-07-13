4a2375b9c9f461ef242de8d02e371d295a48ab53
Marto2023 <matoalfred@gmail.com>
2023-07-13 10:09:48 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,20 +1,14 @@
 #include <stdio.h>
 /**
- * main - A program that prints the size of various comp types
- * Return 0 (Success)
+ * main - Entry
+ * Return: Always 0
  */
 int main(void)
 {
-	char a;
-	int b;
-	long int c;
-	long long int d;
-	float f;
-
-print f("Size of a char: %lu bytes(s)\n", (unsigned long)sizeof(a));
-print f("Size of an int: %lu bytes(s)\n", (unsigned long)sizeof(b));
-print f("Size of a long int: %lu bytes(s)\n", (unsigned long)sizeof(c));
-print f("Size of a long long int: %lu bytes(s)\n", (unsigned long)sizeof(d));
-print f("Size of a float: %lu bytes(s)\n", (unsigned long)sizeof(f));
-return (0);
+	printf("Size of a chair: %i byte(s)\n", sizeof(char));
+	printf("Size of an int: %i byte(s)\n", sizeof(int));
+	printf("Size of a long int: %i byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %i byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %i byte(s)\n", sizeof(float));
+	return (0);
 }
